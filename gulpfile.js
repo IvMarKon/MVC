5e09ec535ea9ad142696b736a8a1e52f3c759215
Ivan <ivan.markov@sigmaukraine.com>
2018-01-09 12:25:21 +0200

    lint fix and add separate tasks for sorts run and patterns run

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    eslint = require('gulp-eslint')
+var gulp = require("gulp"),
+    browserSync = require("browser-sync"),
+    eslint = require("gulp-eslint");
 
-var ROOT = './';
+var ROOT = "./";
 
-gulp.task('lint', function () {
-    return gulp.src(ROOT + 'js/*.js')
+gulp.task("lint", function () {
+    return gulp.src(ROOT + "**/*.js")
     .pipe(eslint({
         globals: [
-            'jQuery',
-            '$'
+            "jQuery",
+            "$"
         ],
         envs: [
-            'browser'
+            "browser"
         ],
         parserOptions: {
             "ecmaVersion": 6,
@@ -20,37 +20,38 @@ gulp.task('lint', function () {
             "ecmaFeatures": {}
         },
         rules: {
-            'no-alert': 1,
-            'no-bitwise': 1,
-            'camelcase': 1,
-            'curly': 1,
-            'eqeqeq': 1,
-            'no-eq-null': 1,
-            'guard-for-in': 1,
-            'no-empty': 1,
-            'no-use-before-define': 1,
-            'no-obj-calls': 2,
-            //'no-unused-vars': 1,
-            'new-cap': 1,
-            'no-shadow': 0,
-            // 'strict': 1,
-            'no-invalid-regexp': 2,
-            'comma-dangle': 2,
-            //'no-undef': 1,
-            'no-new': 1,
-            'no-extra-semi': 1,
-            'no-debugger': 2,
-            'no-caller': 1,
-            'semi': 1,
-            'quotes': 1,
-            'no-unreachable': 2,
-            'fix': true
+            "no-alert": 1,
+            "no-bitwise": 1,
+            "camelcase": 1,
+            "curly": 1,
+            "eqeqeq": 1,
+            "no-eq-null": 1,
+            "guard-for-in": 1,
+            "no-empty": 1,
+            "no-use-before-define": 1,
+            "no-obj-calls": 2,
+            //"no-unused-vars": 1,
+            "new-cap": 1,
+            "no-shadow": 0,
+            // "strict": 1,
+            "no-invalid-regexp": 2,
+            "comma-dangle": 2,
+            //"no-undef": 1,
+            "no-new": 1,
+            "no-extra-semi": 1,
+            "no-debugger": 2,
+            "no-caller": 1,
+            "semi": 1,
+            "quotes": 1,
+            "no-unreachable": 2,
+            "fix": true
         }
     }))
         .pipe(eslint.format());
 });
 
-gulp.task('browser-sync', function () {
+//For sorts folder block
+gulp.task("browser-sync-sorts", function () {
     browserSync({
         server: {
             baseDir: ROOT+"sorts_files"
@@ -59,11 +60,30 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', ['browser-sync', 'lint'], function () {
-    gulp.watch(ROOT+'*.html', browserSync.reload);
-    gulp.watch(ROOT+'sorts_files/js/*.js', ['lint',browserSync.reload]);
+gulp.task("watch-sorts", ["browser-sync-sorts", "lint"], function () {
+    gulp.watch(ROOT+"*.html", browserSync.reload);
+    gulp.watch(ROOT+"sorts_files/js/*.js", ["lint",browserSync.reload]);
 });
 
-gulp.task('debug',function(){
-    gulp.watch(ROOT+'sorts_files/js/*.js', ['lint']);
+gulp.task("debug-sorts",function(){
+    gulp.watch(ROOT+"sorts_files/js/*.js", ["lint"]);
+});
+
+//For patterns folder block
+gulp.task("browser-sync-patterns", function () {
+  browserSync({
+      server: {
+          baseDir: ROOT+"patterns_files"
+      },
+      notify: false
+  });
+});
+
+gulp.task("watch-patterns", ["browser-sync-patterns", "lint"], function () {
+  gulp.watch(ROOT+"*.html", browserSync.reload);
+  gulp.watch(ROOT+"patterns_files/js/*.js", ["lint",browserSync.reload]);
+});
+
+gulp.task("debug-patterns",function(){
+  gulp.watch(ROOT+"patterns_files/js/*.js", ["lint"]);
 });
