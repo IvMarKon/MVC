12de3ca1128cb6ab13d6e34fee9e9a1b28e6b0f5
Ivan <ivan.markov@sigmaukraine.com>
2018-01-16 13:50:43 +0200

    Add binary search console output

--- sorts_files/js/main.js
+++ sorts_files/js/main.js
@@ -8,10 +8,14 @@ var start = Date.now();
 
 (function () {
   //merge-sort / second param - switches log on
-  mergeSort(getRandomArray(25), true);
-  algoCount(start);
+  mergeSort(getRandomArray(25), false);
+  algoCount("Merge sort algorithm",start);
 
   //quick-sort / second param - switches log on
-  quickSort(getRandomArray(25), true);
-  algoCount(start);
+  quickSort(getRandomArray(25), false);
+  algoCount("Quick sort algorithm",start);
+
+  //binary search / second param - item position of what we want to find
+  binarySearch(getOrderedArray(250),201);
+  algoCount("Binary search",start);
 })();
