8ebf0f9800bbc0cc14ab9737faea9061df01f448
Ivan <ivan.markov@sigmaukraine.com>
2018-01-17 16:17:50 +0200

    hash table test functions

---
+++ structures_files/hash_table/js/main.js
@@ -0,0 +1,13 @@
+var hashTable = new HashTable(3);
+hashTable.add('first', 1);
+hashTable.add('second', 2);
+hashTable.add('third', 3);
+hashTable.add('fourth', 4);
+hashTable.add('fifth', 5);
+hashTable.print();
+console.log('hash table length:', hashTable.length());
+console.log('search second:', hashTable.search('second'));
+hashTable.remove('fourth');
+hashTable.remove('first');
+hashTable.print();
+console.log('hash table length:', hashTable.length());
